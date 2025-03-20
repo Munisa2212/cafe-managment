72774fe09afce2b535ec4c499e2b3eb7738886d0
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-03-20 15:34:40 +0500

    payment module done

---
+++ models/payment.module.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize")
+const {db} = require("../config/db")
+const joi = require("joi")
+
+const Payment = db.define(
+    "Payments",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        order_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+    }
+)
+
+const PaymentValidation = joi.object({
+    order_id: joi.number().required(),
+    status: joi.number
+})
+
+module.exports = {Payment, PaymentValidation}
